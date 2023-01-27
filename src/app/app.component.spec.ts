055b1a9a3ae49e1ba825cb54494596082a0ff18b
Carlos Portillo <carlosportillo308@gmail.com>
2023-01-26 19:57:25 -0500

    testing resuelto para las operaciones

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,48 @@
-import { TestBed } from '@angular/core/testing';
-import { FormsModule } from '@angular/forms';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-
+import { FormsModule } from '@angular/forms';
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [
-        FormsModule
-      ],
-      declarations: [
-        AppComponent,
-      ],
-    }).compileComponents();
-  });
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [ FormsModule ],
+      declarations: [ AppComponent ]
+    })
+    .compileComponents();
+  }));
 
-  it(`should have as title 'proyecto27-1'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('proyecto27-1');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('proyecto27-1');
+  });
+  it('should calculate result correctly for sum operation', () => {
+    component.valor1 = 5;
+    component.valor2 = 7;
+    component.operar('suma');
+    expect(component.resultado).toBe(12);
+  });
+
+  it('should calculate result correctly for subtraction operation', () => {
+    component.valor1 = 5;
+    component.valor2 = 7;
+    component.operar('resta');
+    expect(component.resultado).toBe(-2);
+  });
+
+  it('should calculate result correctly for multiplication operation', () => {
+    component.valor1 = 5;
+    component.valor2 = 7;
+    component.operar('multiplicacion');
+    expect(component.resultado).toBe(35);
+  });
+
+  it('should calculate result correctly for division operation', () => {
+    component.valor1 = 14;
+    component.valor2 = 7;
+    component.operar('division');
+    expect(component.resultado).toBe(2);
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  static resultado(resultado: any) {
+    throw new Error('Method not implemented.');
+  }
+  static valor1: number;
+  static valor2: number;
+  static operar(arg0: string) {
+    throw new Error('Method not implemented.');
+  }
   title = 'proyecto27-1';
   valor1: number = 0;
   valor2: number = 0;
   resultado: number = 0;
-  operacion(tipo: string) {
+  operar(tipo: string) {
     if (tipo === 'suma') {
       this.resultado = this.valor1 + this.valor2;
     } else if (tipo === 'resta') {
